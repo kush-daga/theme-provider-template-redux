55598a604af87c35ffbb385a11b81641e0100d76
Kush Daga <kushdaga@hotmail.com>
2020-08-24 20:59:58 +0530

    added boilerplate

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
   return <div className="App"></div>;
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export const darkTheme = {
+  background: "#1f1f1f",
+  backgroundSecondary: "#303030",
+  text: "#bfbebe",
+  textSecondary: "#848886",
+  border: "#272727",
+};
+export const lightTheme = {
+  background: "#EAEAEA",
+  backgroundSecondary: "#F8F8F8",
+  text: "#1f1f1f",
+  textSecondary: "#303030",
+  border: "#EAEAEA",
+};
